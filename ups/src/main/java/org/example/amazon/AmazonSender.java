b8dc75645df08e20a46ca83a9402d9edc50623a1
rx50 <rx50@duke.com>
2023-04-25 15:07:51 -0400

    Created AmazonSender

---
+++ ups/src/main/java/org/example/amazon/AmazonSender.java
@@ -0,0 +1,11 @@
+package org.example.amazon;
+
+public class AmazonSender implements Runnable {
+
+    @Override
+    public void run() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'run'");
+    }
+
+}
